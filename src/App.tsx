1fef3fc9e0dbb8f66686769d6513477796947e89
Ardeniscool1293 <arden.stanley@yahoo.com>
2024-02-20 15:12:21 -0600

    On branch main  Initial commit 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.tsx 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function welcomePage() {
+  return (
+    
+  );
+}
+
+export default function App() {
+  return (
+    <body>
+      <title>InfirmAssist</title>
+      <welcomePage/>
+    </body>
+  );
+}
+
+
+
