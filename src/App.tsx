9355a43f91f2bd0718c11e9cca2003618f5d0a56
Ardeniscool1293 <arden.stanley@yahoo.com>
2024-02-21 16:17:31 -0600

    modified:   src/App.tsx 	modified:   src/index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-function welcomePage() {
+function WelcomePage() {
   return (
-    
+    <body style={{width: screen.width, height: screen.height}}>
+      <h1 className="companyTitle" style={
+        {fontSize: (screen.width/20)}
+      }>InfirmAssist</h1>
+      <h2 className="infoText" style={
+        {fontSize: (screen.width/75)}
+      }>Receptionist and Hospital Administration Software</h2>
+      <button style= {
+        {height: screen.height/10, width: screen.width/10}
+      }>How to use</button>
+      <button style = {
+        {height: screen.height/10, width: screen.width/10}
+      }>Receptionist</button>
+      <button style={
+        {height: screen.height/10, width: screen.width/10}
+      }>Analytics</button>
+    </body>
   );
 }
 
 export default function App() {
   return (
-    <body>
-      <title>InfirmAssist</title>
-      <welcomePage/>
-    </body>
+    <WelcomePage/>
   );
 }
-
-
-
