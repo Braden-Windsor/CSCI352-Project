52bc2cde84d482dc4152b21c6409f9ea372bee66
TreyWrigh03 <dwrigh43@ut.utm.edu>
2024-03-14 03:00:27 -0500

    i got a dolla i got a dolla i got a dolla hey hey hey hey

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-function PatientEntryPage() {
-  return (
-    <body id="patiententry">
-      <h1 className="companyTitle">InfirmAssist</h1>
-      <h2 className="infoText">Patient Entry</h2>
-      <span>
-          <form id="patientEntryInputs" action='/' method="post">
-            <h3 className="inputDescription">Patient First Name</h3>
-              <input type='text' name='firstname'></input>
-            <h3 className="inputDescription">Patient Last Name</h3>
-              <input type='text' name='lastname'></input>
-            <h3 className="inputDescription">Patient SSN</h3>
-              <input type='text' name='ssn'></input>
-            <h3 className="inputDescription">Patient Date of admittance</h3>
-              <input type='text' name='doa'></input>
-            <h3 className="inputDescription">Patient Date of Birth</h3>
-              <input type='text' name='dob'></input>
-            <button id="submit" type='submit'>submit</button>
-          </form>
-      </span>
-    </body>
-  );
-}
-
 function WelcomePage() {
   return (
     <body id="welcome">
@@ -48,12 +24,67 @@ function WelcomePage() {
   );
 }
 
+function PatientEntryPage() {
+  return (
+    <body id="patiententry">
+      <h1 className="companyTitle">InfirmAssist</h1>
+      <h2 className="infoText">Patient Entry</h2>
+      <span>
+          <form id="patientEntryInputs" action='/' method="post">
+            <h3 className="inputDescription">Patient First Name</h3>
+              <input type='text' name='firstname'></input>
+            <h3 className="inputDescription">Patient Last Name</h3>
+              <input type='text' name='lastname'></input>
+            <h3 className="inputDescription">Patient SSN</h3>
+              <input type='text' name='ssn'></input>
+            <h3 className="inputDescription">Patient Date of Admittance</h3>
+              <input type='text' name='doa'></input>
+            <h3 className="inputDescription">Patient Date of Birth</h3>
+              <input type='text' name='dob'></input>
+              <button><span className="spoon"></span>Submit</button>
+          </form>
+      </span>
+    </body>
+  );
+}
+
+function PatientDirectoryPage(){
+  return(
+    <body id="patientdirectory">
+      <div className="container">
+        <h2 className="infoText">Patient Directory</h2>
+        <input type="text" placeholder="Search patients..." className="searchInput"></input>
+        <div className="patientList">
+          {/* This is a temp so i can make progress*/}
+          <div className="patientItem">
+            <h3>Trey Wright</h3>
+            <p>DOB: 12/04/2003</p>
+            <p>SSN: 123-45-6789</p>
+          </div>
+          <div className="patientItem">
+            <h3>Arden Stanley</h3>
+            <p>DOB: 02/31/2003</p>
+            <p>SSN: 922-62-2690</p>
+          </div>
+          <div className="patientItem">
+            <h3>Braden Windsor</h3>
+            <p>DOB: 09/01/2004</p>
+            <p>SSN: 420-65-4481</p>
+          </div>
+          {/* can copy and paste to add more stuff*/}
+        </div>
+      </div>
+    </body>
+  );
+}
+
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<WelcomePage />} />
         <Route path='/PatientEntry' element={<PatientEntryPage />} />
+        <Route path='/PatientDirectory' element={<PatientDirectoryPage />} />
       </Routes>
     </Router>
   );  
